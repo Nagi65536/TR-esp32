5f0df87b4ae4bb1fcbdc85a2b552f0fe3b0bcea9
SatooRu <ryuto65536@gmail.com>
2022-08-03 15:52:05 +0900

    UDPできた

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,65 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUDP.h>
+
+const char ssid[] = "MeidenPad";       // TODO
+const char pass[] = "MeideniPad@0311"; // TODO
+static WiFiUDP wifiUdp;
+static const char *raspberryIP = "10.151.40.4";
+static const int raspberryPort = 9000; //送信先のポート
+
+IPAddress remoteIP; // 相手のIPアドレス
+int port;
+
+static void WiFi_setup()
+{
+  static const int kLocalPort = 7000; //自身のポート
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
+  wifiUdp.begin(kLocalPort);
+}
+
+static void Serial_setup()
+{
+  Serial.begin(115200);
+  Serial.println(""); // to separate line
+}
 
 void setup()
 {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
+  Serial_setup();
+  WiFi_setup();
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for a second
+  int count = 0;
+  char i[64];
+
+  while (1)
+  {
+    count++;
+
+    //パケットの送信
+    wifiUdp.beginPacket(raspberryIP, raspberryPort);
+    wifiUdp.printf("Hello,server?");
+    wifiUdp.endPacket();
+
+    //パケットの受信
+    if (wifiUdp.parsePacket())
+    {
+      wifiUdp.read(i, 64);
+      remoteIP = wifiUdp.remoteIP();
+      port = wifiUdp.remotePort();
+      Serial.print(remoteIP);
+      Serial.print(" / ");
+      Serial.print(port);
+      Serial.print(" / ");
+      Serial.println(i); // UDP通信で来た値を表示
+    }
+
+    delay(3000);
+  }
 }
